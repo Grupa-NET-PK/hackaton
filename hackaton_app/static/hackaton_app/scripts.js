bfd93305076a8d0545d573b9faf1de0745377447
Sebastian <sebastiankluwa@gmail.com>
2020-11-21 13:29:17 +0100

    Assigned flashcard refreshing, sending req, getting response

--- hackaton_app/static/hackaton_app/scripts.js
+++ hackaton_app/static/hackaton_app/scripts.js
@@ -1,14 +1,12 @@
 function runInterval(userid, url){
     var myVar = setInterval(() =>{
-        console.log(userid+"");
         $.ajax({
             url: url,
             data : {
                 'userid': userid
             },
             success: function (data) {
-                //$("#assigned_flashcards").html("+$"+data);
-                console.log(data);
+                $("#assigned_flashcards").html(""+data);
             }
         })
     }, 2000);
